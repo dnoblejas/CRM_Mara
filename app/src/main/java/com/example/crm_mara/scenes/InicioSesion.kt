527035dad6bc1c1aa72ea3b325483a7804302792
dnoblejas59 <ronperri89@gmail.com>
2024-12-02 13:56:01 +0100

    agenda ope

--- app/src/main/java/com/example/crm_mara/scenes/InicioSesion.kt
+++ app/src/main/java/com/example/crm_mara/scenes/InicioSesion.kt
@@ -1,12 +1,31 @@
 package com.example.crm_mara.scenes
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DarkMode
 import androidx.compose.material.icons.filled.WbSunny
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -18,6 +37,7 @@ import androidx.navigation.NavController
 import com.example.crm_mara.ui.theme.ZendotsFamily
 import com.google.firebase.firestore.FirebaseFirestore
 
+//no
 @Composable
 fun InicioSesion(navController: NavController) {
     var nombre by remember { mutableStateOf("") }
